6cc9487a25299ceee02c3cb314352b67addb6092
JarvisBott <96819526+JarvisBott@users.noreply.github.com>
2022-04-03 16:25:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6283106733687'] // Put your number here
+global.owner = ['6281283933929'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -28,11 +28,11 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'WhatsAppBOT-Md'
-global.author = 'By WhatsApp'
-global.email = 'noisyboyss887@gmail.com'
+global.packname = 'JarvisBot'
+global.author = 'By Umar Fajar'
+global.email = 'adhanmod5@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© WhatsAppBOT-Md'
+global.wm = '© JarvisBot'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
